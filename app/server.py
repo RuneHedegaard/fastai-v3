6e525869fba83c9d09ef5c56861b909d7b92fe22
RuneHedegaard <55020190+RuneHedegaard@users.noreply.github.com>
2019-09-09 20:30:39 +0200

    changed URL

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1INTSv8a0olf-hFXsRrBWLI32d8lLdKkI'
 export_file_name = 'export.pkl'
 
 classes = ['black', 'grizzly', 'teddys']
